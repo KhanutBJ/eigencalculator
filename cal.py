a0512cb20529957ecc7df864f48cf5ee1cad70a5
KhanutBJ <95537645+KhanutBJ@users.noreply.github.com>
2023-11-29 02:42:51 +0700

    Update cal.py

--- cal.py
+++ cal.py
@@ -52,7 +52,7 @@ def calculate():
     eigenvalues, eigenvectors = np.linalg.eig(matrix)
     st.write("Eigenvalues")
     root = np.power(num1+num4,2) - 4*int(np.linalg.det(matrix))
-    st.success(f"root: {root}")
+    st.success(f"b^2 - 4ac: {root}")
     
     if  root >= 0:
         values = np.array(eigenvalues).astype(int)
